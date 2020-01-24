b3febd40489bd829331f21d6ed426f0cd6dc6ea9
Marco Langer <marco.langer@kit.edu>
2020-01-24 18:09:39 +0100

    Little changes

--- modernsyntax/range_looping.cpp
+++ modernsyntax/range_looping.cpp
@@ -8,7 +8,7 @@ g++ -o range_looping -std=c++14 -pthread -g -Wall -Wextra -Wpedantic -Werror ran
 
 class Tool {
 public:
-  Tool(): aMember(0) { 
+  Tool(): aMember(0) {
     std::this_thread::sleep_for (std::chrono::milliseconds(500));
   };
   Tool(const Tool&) {
@@ -16,13 +16,12 @@ public:
   }
   void print() const { std::cout << "tool" << std::endl; };
 
-protected:
+private:
   int aMember;
 };
 
-
 void rangeLoop(std::vector<Tool>& tools){
-  for(auto& tool : tools){
+  for(const auto& tool : tools){
     tool.print();
   }
 }
@@ -31,7 +30,7 @@ int main(){
   // create a vector of 5 Tools
   std::cout << "Creating tools" << std::endl;
   std::vector<Tool> tools(5);
-  
+
   std::cout << "Start range looping" << std::endl;
   rangeLoop(tools);
 
